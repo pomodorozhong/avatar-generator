b83e7d99d984b45421d0bf3f3747f7cc96247190
pomodorozhong <pomodorozhong@gmail.com>
2020-07-18 22:06:11 +0800

    feat✨(*): add canvas, and draw a little on it

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,29 @@
-console.log("Hello World");
+let canvas = document.getElementsByTagName("canvas")[0];
+canvas.width = 480;
+canvas.height = 480;
+let ctx: CanvasRenderingContext2D = canvas.getContext("2d");
+
+draw();
+
+function draw() {
+    let width = 50;
+    let height = width;
+    
+    let x = 50;
+    let y = 50;
+    // upper left to lower right
+    for (let index = 0; index < 34; index++) {
+        ctx.strokeRect(x, y, width, height);
+        x += 10;
+        y += 10;
+    }
+
+    x = 50;
+    y = 480 - 100;
+    // upper left to lower right
+    for (let index = 0; index < 34; index++) {
+        ctx.strokeRect(x, y, width, height);
+        x += 10;
+        y -= 10;
+    }
+}
