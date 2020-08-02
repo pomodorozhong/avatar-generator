f99253282ab1b13120430c3119e72a791f09631c
pomodorozhong <pomodorozhong@gmail.com>
2020-08-03 00:48:02 +0800

    feat✨(view): using ControlGenerator

--- src/view.ts
+++ src/view.ts
@@ -1,56 +1,59 @@
 import { Presenter } from "./presenter";
+import { ControlGenerator } from "./controlGenerator";
 
 export class View {
-    presenter: Presenter;
-    canvas: HTMLCanvasElement;
-    constructor(presenter: Presenter) {
-        this.presenter = presenter;
-        this.canvas = document.getElementsByTagName("canvas")[0];
+  presenter: Presenter;
+  canvas: HTMLCanvasElement;
+  controlGenerator: ControlGenerator;
 
-        let size: number = 480;
-        this.canvas.width = size;
-        this.canvas.height = size;
+  constructor(presenter: Presenter) {
+    this.presenter = presenter;
+    this.controlGenerator = new ControlGenerator(this.presenter);
+    this.canvas = document.getElementsByTagName("canvas")[0];
 
-        this.initialPatternSelection();
-        this.hookEventListeners();
+    let size: number = 480;
+    this.canvas.width = size;
+    this.canvas.height = size;
+
+    this.initialPatternSelection();
+    this.hookEventListeners();
+    this.settingHandler();
+  }
+
+  initialPatternSelection() {
+    let input: HTMLInputElement = <HTMLInputElement>(
+      document.getElementById("ddl_select_pattern")
+    );
+    input.addEventListener("change", pattern_selected, false);
+
+    let pattern_list = this.presenter.getPatternList();
+    for (let index = 0; index < pattern_list.length; index++) {
+      const pattern = pattern_list[index];
+      var opt = document.createElement("option");
+      opt.appendChild(document.createTextNode(pattern));
+      input.appendChild(opt);
     }
 
-    initialPatternSelection() {
-        let input: HTMLInputElement = <HTMLInputElement>(
-            document.getElementById("ddl_select_pattern")
-        );
-        input.addEventListener("change", pattern_selected, false);
-
-        let pattern_list = this.presenter.getPatternList();
-        for (let index = 0; index < pattern_list.length; index++) {
-            const pattern = pattern_list[index];
-            var opt = document.createElement("option");
-            opt.appendChild(document.createTextNode(pattern));
-            input.appendChild(opt);
-        }
-
-        let self = this;
-        function pattern_selected(e: any) {
-            let pattern = e.target.value;
-            self.presenter.selectPattern(pattern);
-            
-            let setting = self.presenter.getSelectedPatternSetting();
-            self.settingHandler(setting);
-        }
+    let self = this;
+    function pattern_selected(e: any) {
+      let pattern = e.target.value;
+      self.presenter.selectPattern(pattern);
+      self.settingHandler();
     }
+  }
 
-    hookEventListeners() {
-        document.getElementById("btn_generate").addEventListener("click", draw);
+  hookEventListeners() {
+    document.getElementById("btn_generate").addEventListener("click", draw);
 
-        let self = this;
-        function draw() {
-            self.presenter.draw(self.canvas);
-        }
+    let self = this;
+    function draw() {
+      self.presenter.draw(self.canvas);
     }
+  }
 
-    settingHandler(setting) {
-        console.log("settingHandler()  not implemented");
+  settingHandler() {
+    let container = document.getElementById("setting_container");
 
-        // TODO: using setting to generate all kinds of input element
-    }
+    this.controlGenerator.updateSettingControl(container);
+  }
 }
