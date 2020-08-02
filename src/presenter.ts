a20b05a1cdf40dbb563a915943c61c8984ec6bdc
pomodorozhong <pomodorozhong@gmail.com>
2020-08-03 00:43:38 +0800

    feat✨(presenter): add two functions for setting pattern

--- src/presenter.ts
+++ src/presenter.ts
@@ -2,27 +2,36 @@ import { View } from "./view";
 import { PatternManager } from "./patternManager";
 
 export class Presenter {
-    view: View;
-    patternManager: PatternManager;
+  view: View;
+  patternManager: PatternManager;
 
-    constructor() {
-        this.patternManager = new PatternManager();
-        this.view = new View(this);
-    }
+  constructor() {
+    this.patternManager = new PatternManager();
+    this.view = new View(this);
+  }
 
-    selectPattern(selection: string) {
-        this.patternManager.selected = selection;
-    }
+  selectPattern(selection: string) {
+    this.patternManager.selected = selection;
+  }
 
-    getPatternList(): Array<string> {
-        return this.patternManager.patternNames;
-    }
+  getPatternList(): Array<string> {
+    return this.patternManager.patternNames;
+  }
 
-    getSelectedPatternSetting(): Record<string, any> {
-        return this.patternManager.selectedSetting;
-    }
+  getSelectedPatternName(): string {
+    return this.patternManager.selected;
+  }
 
-    draw(canvas: HTMLCanvasElement) {
-        this.patternManager.draw(canvas);
-    }
+  getSelectedPatternSetting(): Record<string, any> {
+    return this.patternManager.selectedSetting;
+  }
+
+  setSelectedPatternSetting(settingName: string, value: any) {
+    this.patternManager.selectedSetting[settingName] = value;
+    // TODO: error handling
+  }
+
+  draw(canvas: HTMLCanvasElement) {
+    this.patternManager.draw(canvas);
+  }
 }
