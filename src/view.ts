f27842d44aec16ee113b1e1bfe5fceaeabe61e93
pomodorozhong <pomodorozhong@gmail.com>
2020-08-06 20:56:48 +0800

    feat✨(view): add canvas placeholder

--- src/view.ts
+++ src/view.ts
@@ -2,58 +2,75 @@ import { Presenter } from "./presenter";
 import { ControlGenerator } from "./controlGenerator";
 
 export class View {
-  presenter: Presenter;
-  canvas: HTMLCanvasElement;
-  controlGenerator: ControlGenerator;
+    presenter: Presenter;
+    canvas: HTMLCanvasElement;
+    controlGenerator: ControlGenerator;
 
-  constructor(presenter: Presenter) {
-    this.presenter = presenter;
-    this.controlGenerator = new ControlGenerator(this.presenter);
-    this.canvas = document.getElementsByTagName("canvas")[0];
+    constructor(presenter: Presenter) {
+        this.presenter = presenter;
+        this.controlGenerator = new ControlGenerator(this.presenter);
+        this.canvas = document.getElementsByTagName("canvas")[0];
 
-    let size: number = 480;
-    this.canvas.width = size;
-    this.canvas.height = size;
+        let size: number = 480;
+        this.canvas.width = size;
+        this.canvas.height = size;
 
-    this.initialPatternSelection();
-    this.hookEventListeners();
-    this.settingHandler();
-  }
-
-  initialPatternSelection() {
-    let input: HTMLInputElement = <HTMLInputElement>(
-      document.getElementById("ddl_select_pattern")
-    );
-    input.addEventListener("change", pattern_selected, false);
-
-    let pattern_list = this.presenter.getPatternList();
-    for (let index = 0; index < pattern_list.length; index++) {
-      const pattern = pattern_list[index];
-      var opt = document.createElement("option");
-      opt.appendChild(document.createTextNode(pattern));
-      input.appendChild(opt);
+        this.initialPatternSelection();
+        this.hookEventListeners();
+        this.settingHandler();
+        this.drawCanvasPlaceholder();
     }
 
-    let self = this;
-    function pattern_selected(e: any) {
-      let pattern = e.target.value;
-      self.presenter.selectPattern(pattern);
-      self.settingHandler();
+    initialPatternSelection() {
+        let input: HTMLInputElement = <HTMLInputElement>(
+            document.getElementById("ddl_select_pattern")
+        );
+        input.addEventListener("change", pattern_selected, false);
+
+        let pattern_list = this.presenter.getPatternList();
+        for (let index = 0; index < pattern_list.length; index++) {
+            const pattern = pattern_list[index];
+            var opt = document.createElement("option");
+            opt.appendChild(document.createTextNode(pattern));
+            input.appendChild(opt);
+        }
+
+        let self = this;
+        function pattern_selected(e: any) {
+            let pattern = e.target.value;
+            self.presenter.selectPattern(pattern);
+            self.settingHandler();
+        }
     }
-  }
 
-  hookEventListeners() {
-    document.getElementById("btn_generate").addEventListener("click", draw);
+    hookEventListeners() {
+        document.getElementById("btn_generate").addEventListener("click", draw);
 
-    let self = this;
-    function draw() {
-      self.presenter.draw(self.canvas);
+        let self = this;
+        function draw() {
+            self.presenter.draw(self.canvas);
+        }
     }
-  }
 
-  settingHandler() {
-    let container = document.getElementById("setting_container");
+    settingHandler() {
+        let container = document.getElementById("setting_container");
 
-    this.controlGenerator.updateSettingControl(container);
-  }
+        this.controlGenerator.updateSettingControl(container);
+    }
+
+    drawCanvasPlaceholder() {
+        let ctx = this.canvas.getContext("2d");
+        ctx.font = "1em serif";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+
+        let width = this.canvas.width;
+        let height = this.canvas.height;
+        ctx.fillText(`${width}x${height}`, width / 2, height / 2);
+        ctx.fillText(
+            'Click "Generate!" to generate things.',
+            width / 2,
+            height / 2 + 20
+        );
+    }
 }
